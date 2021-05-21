f29df4307935322a450106d1aaa7cc3f70759aa4
Sumedha J A <sumedhasmileok@gmail.com>
2021-05-21 12:44:02 +0530

    presentation page added

--- gesture_app/urls.py
+++ gesture_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('navigate/', views.Navigate, name='navigate'),
+    path('navigate_vedio/', views.Navigate_Vedio, name='navigate'),
+    path('navigate_ppt/', views.Navigate_Player, name='navigate_ppt'),
 ]
--- gesture_app/views.py
+++ gesture_app/views.py
@@ -5,7 +5,7 @@ import pyautogui as pg
 def index(request):
     return render(request, "HomePage.html")
 
-def Navigate(request):
+def Navigate_Vedio(request):
     pg.hotkey('winleft')
     pg.typewrite('Movies\n',0.2)
     pg.press('enter')
@@ -18,4 +18,17 @@ def Navigate(request):
     pg.write('f:\n',0.2)
     pg.write('cd F:\Final Year Project\Hand Gesture3\n',0.2)
     pg.write('python hand_gesture_youtube.py\n',0.2)
+    return redirect('index')
+
+def Navigate_Player(request):
+    pg.hotkey('winleft')
+    pg.typewrite('PowerPoint\n',0.2)
+    pg.press('enter')
+
+    pg.hotkey('winleft')
+    pg.typewrite('Command\n',0.2)
+    pg.press('enter')
+    pg.write('f:\n',0.2)
+    pg.write('cd F:\Final Year Project\Hand Gesture3\n',0.2)
+    pg.write('python power_point.py\n',0.2)
     return redirect('index')
