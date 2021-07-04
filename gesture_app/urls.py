05bfa026f53094de7ecff74e3611d5e86272368b
Sumedha J A <sumedhasmileok@gmail.com>
2021-07-04 13:57:20 +0530

    front end added

--- gesture_app/urls.py
+++ gesture_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('navigate_vedio/', views.Navigate_Vedio, name='navigate'),
     path('navigate_ppt/', views.Navigate_Player, name='navigate_ppt'),
     path('navigate_paint/',views.PaintWindow,name='navigate_paint'),
+    path('home',views.Home,name='home'),
 ]
--- gesture_app/views.py
+++ gesture_app/views.py
@@ -11,115 +11,102 @@ from . import HandTrackingModule as htm
 ########################
 brushThickness =10
 eraserThickness =100
+wCam,hCam = 640,480
 ########################
 # Create your views here.
 def index(request):
-    return render(request, "HomePage.html")
+    return render(request, "index.html")
 
 
 
 def Navigate_Vedio(request):
+    pTime =0
     pg.hotkey('winleft')
     pg.sleep(1)
     pg.typewrite('Movies\n',0.4)
     pg.press('enter')
-    cap = cv2.VideoCapture(0)
-    while(cap.isOpened()):
-
-        ret ,img = cap.read()
-
-        cv2.rectangle(img, (300,300),(100,100), (0,255,0),0)
-        crop_img = img[100:300, 100:300]
-
-        grey = cv2.cvtColor(crop_img,cv2.COLOR_BGR2GRAY)
-
-        value = (35,35)
-        blurred = cv2.GaussianBlur(grey,value,0)
-
-        _, thresh1 = cv2.threshold(blurred,127,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-        cv2.imshow('thresholded',thresh1)
-
-        (version, _, _) = cv2.__version__.split('.')
-
-        if version == '3':
-            image,contours,hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE,
-                                                cv2.CHAIN_APPROX_NONE)
-        elif version == '4':
-            contours,hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE,
-                                                cv2.CHAIN_APPROX_NONE)
-
-        cnt = max(contours, key=lambda x: cv2.contourArea(x))
-
-        x,y,w,h = cv2.boundingRect(cnt)
-        cv2.rectangle(crop_img,(x,y), (x+w, y+h), (0,0,255),0)
-
-        hull = cv2.convexHull(cnt)
-
-        drawing = np.zeros(crop_img.shape,np.uint8)
-        cv2.drawContours(drawing,[cnt],0,(0,255,0),0)
-        cv2.drawContours(drawing,[hull],0,(0,255,0),0)
-
-        hull = cv2.convexHull(cnt, returnPoints=False)
-
-        defects = cv2.convexityDefects(cnt,hull)
-        count_defects = 0
-        cv2.drawContours(thresh1,contours,-1,(0,255,0),3)
+    cap=cv2.VideoCapture(0)
+    cap.set(3,wCam)
+    cap.set(4,hCam)
 
 
-        for i in range(defects.shape[0]):
-            s,e,f,d = defects[i,0]
+
+    folderPath = "F:\Final Year Project\Hand-Gesture-Web\hand_gesture_app\gesture_app\FingerImages"
+    myList = os.listdir(folderPath)
+    print(myList)
+    overlayList =[]
+    for imPath in myList:
+        image = cv2.imread(f'{folderPath}/{imPath}')
+        overlayList.append(image)
+    print(len(overlayList))
+
+    detector = htm.handDetector(detectionCon=0.75)
+    
+    tipIds = [4,8,12,16,20]
+    while True:
+        success,img =cap.read()
+        # img = cv2.flip(img,1)
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img,draw=False)
+    
+
+        if len(lmList) != 0:
+            fingers = []
 
 
-            start = tuple(cnt[s][0])
-            end = tuple(cnt[e][0])
-            far = tuple(cnt[f][0])
-            #Cosin Rule
-            
-            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-            angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
+            #Thumb
+            if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
+                    fingers.append(1)
+            else:
+                    fingers.append(0)
 
+            # 4 fingers
 
-            if angle < 90:
-                count_defects += 1
-                cv2.circle(crop_img,far,3,[0,0,255],-1)
-            
-            cv2.line(crop_img,start,end,[0,255,0], 2)
+            for id in range(1,5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            # print(fingers)      
+            totalFingers = fingers.count(1) 
+            print(totalFingers)
+
+            h,w,c = overlayList[totalFingers-1].shape
+            img[0:h,0:w] = overlayList[totalFingers-1] 
+
+            if totalFingers == 1:
+                pg.press("space",interval=1)              
+                cv2.putText(img, "Play/Pause",(400,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,0,0), 3)
         
-        if count_defects == 1:
-            pg.press("space",interval=1)              
-            cv2.putText(img, "Play/Pause", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 10)
-            
+            elif totalFingers == 2:
+                pg.press("volumeup")
+                cv2.putText(img, "Volume Up",(400,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,0,0), 3)
+
+            elif totalFingers == 3:
+                pg.press("volumedown")
+                cv2.putText(img, "Volume Down",(400,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,0,0), 3)
+
+
+            elif totalFingers == 4:
+                pg.press("volumemute",interval=1)
+                cv2.putText(img, "Mute/Unmute",(400,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,0,0), 3)
+
+            else:
+                cv2.putText(img,"NONE",(400,70),cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),10)
         
-        elif count_defects == 2:
-            pg.press("volumeup")
-            cv2.putText(img, "Volume Up", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 10)
-            
-        elif count_defects == 3:
-            pg.press("volumedown")
-            cv2.putText(img, "Volume Down", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 10)  
-
-        elif count_defects == 4:
-            pg.press("volumemute",interval=1)
-            cv2.putText(img, "Mute/Unmute", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 10)
-
-        else:
-            cv2.putText(img,"NONE",(50,50),cv2.FONT_HERSHEY_SIMPLEX,2,10)
-            
-
-        cv2.imshow("Gesture", img)
-        all_img = np.hstack((drawing, crop_img))
-        cv2.imshow("Contours", all_img)
-
+        cv2.imshow("Image",img)
         #Help Window
         path = r'F:\Final Year Project\Hand-Gesture-Web\hand_gesture_app\gesture_app\Header\help-vedio.jpg'
         image = cv2.imread(path)
         help = 'image'
         cv2.resizeWindow(help,247,349)
-        cv2.imshow(help, image)   
-                   
+        cv2.imshow(help, image)
+        # cTime = time.time()
+        # fps = 1/(cTime-pTime)
+        # pTime = cTime
+        # cv2.putText(img,f'FPS: {int(fps)}',(400,70),cv2.FONT_HERSHEY_PLAIN,3, (255,0,0),3)
+
+        
 
         if cv2.waitKey(5) == 13:  # if I press Enter it will break 
             break
@@ -133,104 +120,87 @@ def Navigate_Player(request):
     pg.typewrite('PowerPoint\n',0.5)
     pg.press('enter')
     
-    cap = cv2.VideoCapture(0)
-    while(cap.isOpened()):
-        ret ,img = cap.read()
-
-        cv2.rectangle(img, (300,300),(100,100), (0,255,0),0)
-        crop_img = img[100:300, 100:300]
-
-        grey = cv2.cvtColor(crop_img,cv2.COLOR_BGR2GRAY)
-
-        value = (35,35)
-        blurred = cv2.GaussianBlur(grey,value,0)
-
-        _, thresh1 = cv2.threshold(blurred,127,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-        cv2.imshow('thresholded',thresh1)
-
-        (version, _, _) = cv2.__version__.split('.')
-
-        if version == '3':
-            image,contours,hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-        elif version == '4':
-            contours,hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-            
-
-        cnt = max(contours, key=lambda x: cv2.contourArea(x))
-
-        x,y,w,h = cv2.boundingRect(cnt)
-        cv2.rectangle(crop_img,(x,y), (x+w, y+h), (0,0,255),0)
-
-        hull = cv2.convexHull(cnt)
-
-        drawing = np.zeros(crop_img.shape,np.uint8)
-        cv2.drawContours(drawing,[cnt],0,(0,255,0),0)
-        cv2.drawContours(drawing,[hull],0,(0,255,0),0)
-
-        hull = cv2.convexHull(cnt, returnPoints=False)
-
-        defects = cv2.convexityDefects(cnt,hull)
-        count_defects = 0
-        cv2.drawContours(thresh1,contours,-1,(0,255,0),3)
-
-        for i in range(defects.shape[0]):
-            s,e,f,d = defects[i,0]
+    cap=cv2.VideoCapture(0)
+    cap.set(3,wCam)
+    cap.set(4,hCam)
 
 
-            start = tuple(cnt[s][0])
-            end = tuple(cnt[e][0])
-            far = tuple(cnt[f][0])
-            #Cosin Rule
-            
-            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-            angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
+    folderPath = "F:\Final Year Project\Hand-Gesture-Web\hand_gesture_app\gesture_app\FingerImages"
+    myList = os.listdir(folderPath)
+    print(myList)
+    overlayList =[]
+    for imPath in myList:
+        image = cv2.imread(f'{folderPath}/{imPath}')
+        overlayList.append(image)
+    print(len(overlayList))
+
+    detector = htm.handDetector(detectionCon=0.75)
 
 
-            if angle < 90:
-                    count_defects += 1
-                    cv2.circle(crop_img,far,3,[0,0,255],-1)
+    tipIds = [4,8,12,16,20]
+    while True:
+        success,img =cap.read()
+        # img = cv2.flip(img,1)
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img,draw=False)
+    
 
-            cv2.line(crop_img,start,end,[0,255,0], 2)
-        if count_defects == 1:
-            pg.press("left",interval=0.5)              
-            cv2.putText(img, "Left", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 10)
-        elif count_defects == 2:
-            pg.press("right",interval=0.5)              
-            cv2.putText(img, "Right", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 10)
-        elif count_defects == 3:
-            cv2.putText(img, "Escape", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 10)
-            pg.press('sleep',interval=1)
-            pg.press("esc")
-            break
+        if len(lmList) != 0:
+            fingers = []
+
+
+            #Thumb
+            if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
+                    fingers.append(1)
+            else:
+                    fingers.append(0)
+
+            # 4 fingers
+
+            for id in range(1,5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            # print(fingers)      
+            totalFingers = fingers.count(1) 
+            print(totalFingers)
+
+            h,w,c = overlayList[totalFingers-1].shape
+            img[0:h,0:w] = overlayList[totalFingers-1] 
+
+            if totalFingers == 1:
+                pg.press("left",interval=0.5)              
+                cv2.putText(img, "Left",(400,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,0,0), 3)
         
-        else:
-            cv2.putText(img,"NONE",(50,50),cv2.FONT_HERSHEY_SIMPLEX,2,2)
-            
+            elif totalFingers == 2:
+                pg.press("right",interval=0.5)
+                cv2.putText(img, "Right",(400,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,0,0), 3)
 
-        cv2.imshow("Gesture", img)
-        all_img = np.hstack((drawing, crop_img))
-        cv2.imshow("Contours", all_img)
+            elif totalFingers == 3:
+                cv2.putText(img, "Escape",(400,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,0,0), 3)
+                pg.press("sleep",interval=1)
+                pg.press("esc")
+                break
+
+            else:
+                cv2.putText(img,"NONE",(400,70),cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),10)
+
         #Help Window
         path = r'F:\Final Year Project\Hand-Gesture-Web\hand_gesture_app\gesture_app\Header\help-presentation.jpg'
         image = cv2.imread(path)
         help = 'image'
         cv2.resizeWindow(help,240,289)
-        cv2.imshow(help, image)   
+        cv2.imshow(help, image)
+        cv2.imshow("Image",img)
 
-
-        key = cv2.waitKey(10) & 0xFF    
-        if key == 27: 
+        if cv2.waitKey(5) == 13:  # if I press Enter it will break 
             break
     cap.release()
     cv2.destroyAllWindows()        
     return redirect('index')
 
 def PaintWindow(request):
-
-
 
     folderPath = "F:\Final Year Project\Hand-Gesture-Web\hand_gesture_app\gesture_app\Header"
     myList = os.listdir(folderPath)
@@ -338,3 +308,7 @@ def PaintWindow(request):
     cap.release()
     cv2.destroyAllWindows()
     return redirect('index')
+
+
+def Home(request):
+    return render(request,'index.html')
